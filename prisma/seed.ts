0c0ba674e265226032dfc71398e8d6727df14bc4
TK <t.koprivnik@gmail.com>
2024-03-18 20:07:52 +0100

    Update prisma seed, add ts-node and remove dev.db from gitignore

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,16 +1,14 @@
-import { PrismaClient } from '@prisma/client';
+const { PrismaClient } = require('@prisma/client');
 
 const prisma = new PrismaClient();
 
 async function main() {
-  // Create an array of promises for each todo item you want to create
   const todos = [
     { title: 'First todo', complete: false },
     { title: 'Second todo', complete: true },
     // Add more todos as needed
   ].map((todo) => prisma.todo.create({ data: todo }));
 
-  // Execute all promises
   await Promise.all(todos);
 
   console.log('Database has been seeded.');
